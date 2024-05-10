d48fbcf976d0b32680b08a5b1c181ef731ab2a43
ShashankDeepak <shashankdeepak3@gmail.com>
2024-05-10 17:41:13 +0530

    Fixed redirecting issue after login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
@@ -9,27 +9,22 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'View/home_screen.dart';
 import 'View/subscreen/authenticat/login.dart';
 
-void main()  {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-      DevicePreview(
-          enabled: true,
-          tools: const [
-            ...DevicePreview.defaultTools,
-
-          ],
-       builder: (BuildContext context) => MyApp(),
-      )
-  );
+  runApp(DevicePreview(
+    enabled: true,
+    tools: const [
+      ...DevicePreview.defaultTools,
+    ],
+    builder: (BuildContext context) => MyApp(),
+  ));
 }
 
 //app look like
 //https://dribbble.com/shots/18612909-Fitness-Mobile-App/attachments/13805828?mode=media
 
 class MyApp extends StatelessWidget {
-
-  final Future<FirebaseApp> _initialisation=Firebase.initializeApp();
-
+  final Future<FirebaseApp> _initialisation = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +33,11 @@ class MyApp extends StatelessWidget {
 
     return FutureBuilder(
         future: _initialisation,
-        builder: (context,snapshot){
-          if(snapshot.hasError){
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
             print("something went wrong");
-
-
           }
-          if(snapshot.connectionState==ConnectionState.done){
+          if (snapshot.connectionState == ConnectionState.done) {
             return ScreenUtilInit(
                 designSize: const Size(392.72, 834.90),
                 minTextAdapt: true,
@@ -60,23 +53,17 @@ class MyApp extends StatelessWidget {
                     theme: ThemeData(
                       primarySwatch: Colors.grey,
                     ),
-                    initialRoute: '/login',
-
+                    initialRoute: FirebaseAuth.instance.currentUser == null
+                        ? LoginScreen.routeName
+                        : HomeScreen.routeName,
                     routes: {
-                      LoginScreen.routeName:(context)=>LoginScreen(),
-                      HomeScreen.routeName:(context)=>HomeScreen()
-
+                      LoginScreen.routeName: (context) => LoginScreen(),
+                      HomeScreen.routeName: (context) => HomeScreen()
                     },
                   );
                 });
           }
           return CircularProgressIndicator();
-
-        }
-    );
-
-
-
-
+        });
   }
 }
